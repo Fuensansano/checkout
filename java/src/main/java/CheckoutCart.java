156a77e509e02b857c9f97f3a0204a396fe3e562
FuenSansano <i.fuensanosano@gmail.com>
2022-06-04 17:43:17 +0200

    making pass add_item_to_checkout_cart test

--- java/src/main/java/CheckoutCart.java
+++ java/src/main/java/CheckoutCart.java
@@ -1,9 +1,19 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class CheckoutCart {
+  Map <String,Double> items = new HashMap<>();
 
   public Double total() {
-    return 0d;
+    if (items.isEmpty()) {
+      return 0.0;
+    }
+     return items.get("carrot");
   }
 
   public void addItem(String carrot, Double i) {
+      items.put(carrot, i);
   }
 }
